5ee51277b6b245435041dbfea86ff4de46799504
toberge <toberge@stud.ntnu.no>
2019-09-03 16:16:07 +0200

    first version of sql init script, testing get&post requests

---
+++ init.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS articles, users, ratings, comments;
+
+CREATE TABLE articles(
+    article_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    media VARCHAR(2083),
+    content TEXT NOT NULL,
+    upload_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    importance INT NOT NULL DEFAULT 1,
+    category ENUM('innenriks', 'utenriks', 'død') NOT NULL,
+    /*CONSTRAINT PRIMARY KEY article_pk(upload_time, title),*/
+    INDEX article_title_time(title, upload_time),
+    INDEX article_importance_time(importance, upload_time)
+) DEFAULT CHAR SET utf8 DEFAULT COLLATE utf8_general_ci;
+
+CREATE TABLE users(
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+) DEFAULT CHAR SET utf8 DEFAULT COLLATE utf8_general_ci;
+
+CREATE TABLE ratings(
+    article_id INT REFERENCES articles(article_id),
+    user_id INT REFERENCES users(user_id),
+    value INT,
+    CONSTRAINT PRIMARY KEY comment_pk(article_id, user_id)
+);
+
+DROP VIEW IF EXISTS articles_view;
+
+CREATE VIEW articles_view AS(
+    SELECT articles.article_id,
+           title,
+           media,
+           content,
+           upload_time,
+           importance,
+           category,
+           AVG(ratings.value) AS rating
+    FROM articles, ratings
+    WHERE articles.article_id = ratings.article_id
+      AND articles.importance = 1
+    GROUP BY articles.article_id
+    ORDER BY upload_time DESC
+);
+
+CREATE TABLE comments(
+    article_id INT REFERENCES articles(article_id),
+    user_id INT REFERENCES users(user_id),
+    title VARCHAR(30),
+    content TEXT,
+    upload_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT PRIMARY KEY comment_pk(article_id, user_id)
+) DEFAULT CHAR SET utf8 DEFAULT COLLATE utf8_general_ci;
+
+INSERT INTO articles(title, media, content, importance, category) VALUES
+                    ('Title', NULL, 'Text is very <em>italic</em>', 2, 'død');
+INSERT INTO users(name) VALUES ('Donaldo Ducke');
+INSERT INTO ratings(article_id, user_id, value) VALUES (1, 1, 2);
--- server.js
+++ server.js
@@ -1,4 +1,117 @@
+'use strict';
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 const fs = require('file-system');
 const conf = JSON.parse(fs.readFileSync('db.json', 'utf8'));
 
-console.log(conf);
+const pool = mysql.createPool(conf);
+
+app.get('/articles', (req, res) => {
+    console.log('got /articles request');
+    pool.getConnection((err, connection) => {
+        console.log('Got connection!');
+        if (err) {
+            console.log(err, 'connection failure');
+            res.json({ error: 'failed to establish DB connection' });
+        } else {
+            connection.query(
+                'SELECT * FROM articles',
+                (err, rows) => {
+                    connection.release();
+                    if (err) {
+                        console.log(err, 'query error');
+                        res.json({ error: 'query error' });
+                    } else {
+                        console.log(rows);
+                        res.json(rows);
+                    }
+                }
+            ); // end query
+        }
+    }); // end getCOnnection
+});
+
+app.get('/articles/top', (req, res) => {
+    console.log('got /articles/top request');
+    pool.getConnection((err, connection) => {
+        console.log('Got connection!');
+        if (err) {
+            console.log(err, 'connection failure');
+            res.json({ error: 'failed to establish DB connection' });
+        } else {
+            connection.query(
+                'SELECT * FROM articles_view',
+                (err, rows) => {
+                    connection.release();
+                    if (err) {
+                        console.log(err, 'query error');
+                        res.json({ error: 'query error' });
+                    } else {
+                        console.log(rows);
+                        res.json(rows);
+                    }
+                }
+            ); // end query
+        }
+    }); // end getCOnnection
+});
+
+app.post('/users', (req, res) => {
+    console.log('got /users request');
+    pool.getConnection((err, connection) => {
+        console.log('Got connection!');
+        if (err) {
+            console.log(err, 'connection failure');
+            res.json({ error: 'failed to establish DB connection' });
+        } else {
+            connection.query(
+                'INSERT INTO users(name) VALUES (?)',
+                req.body.name,
+                err => {
+                    connection.release();
+                    if (err) {
+                        console.log(err, 'query error');
+                        res.json({ error: 'query error' });
+                    } else {
+                        console.log('POST request succeeded');
+                        res.send('POST successful');
+                    }
+                }
+            ); // end query
+        }
+    }); // end getCOnnection
+});
+
+app.get('/users', (req, res) => {
+    console.log('got /users request');
+    pool.getConnection((err, connection) => {
+        console.log('Got connection!');
+        if (err) {
+            console.log(err, 'connection failure');
+            res.json({ error: 'failed to establish DB connection' });
+        } else {
+            connection.query(
+                'SELECT * FROM users',
+                (err, rows) => {
+                    connection.release();
+                    if (err) {
+                        console.log(err, 'query error');
+                        res.json({ error: 'query error' });
+                    } else {
+                        console.log(rows);
+                        res.json(rows);
+                    }
+                }
+            ); // end query
+        }
+    }); // end getCOnnection
+});
+
+console.log(conf.host);
+
+let server = app.listen(8080);
